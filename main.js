b3f16ac3a2346057cbb71121746af1686c996a55
jwwals1 <joe_walsh59@yahoo.com>
2024-01-03 11:00:01 -0600

    main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function dropDown() {
+    let click = document.getElementById('myDropdowns');
+    if (click.style.display === "block") {
+        click.style.display = "none";
+    } else {
+        click.style.display="block";
+    }
+}
